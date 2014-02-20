c093f4efb256d172ed58314c4bd04479d6e0d35d
WritingPanda <omar.quimbaya4@gmail.com>
2014-02-19 18:19:33 -0600

    Bug fixes and adding feature which output the manner of which the user had input the input

--- palindrome.php
+++ palindrome.php
@@ -3,11 +3,15 @@
 // Create a function to check if a word is a palindrome or not.
 // Amore, Roma .. A man, a plan, a canal: Panama .. No 'x' in 'Nixon
 echo "Check if a word is a palindrome! Enter word: ";
-$input = trim(fgets(STDIN));
+$input1 = trim(fgets(STDIN));
+$input2 = strtoupper($input1);
 
 function is_palindrome($input) {
+	$input = preg_replace("/[^A-Za-z0-9 ]/", "", $input);
+	$input = str_replace(" ", "", $input);
 	$pali = str_split($input);
 	$revpali = array_reverse($pali);
+
 	if ($pali == $revpali) {
 		return true;
 	} else {
@@ -15,9 +19,9 @@ function is_palindrome($input) {
 	}
 }
 
-if (is_palindrome($input)) {
-	echo "{$input} is a palindrome! Cool, huh?\n";
+if (is_palindrome($input2)) {
+	echo "{$input1} is a palindrome! Cool, huh?\n";
 } else {
-	echo "{$input} is not a palindrome. Do you even know what those are?\n";
+	echo "{$input1} is not a palindrome. Do you even know what those are?\n";
 }
 ?>
