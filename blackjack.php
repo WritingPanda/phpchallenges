5c25bf825a78e77089a27d7d18e1c75b9d8c367d
WritingPanda <omar.quimbaya4@gmail.com>
2014-02-23 18:41:16 -0600

    Working on blackjack

--- blackjack.php
+++ blackjack.php
@@ -25,7 +25,8 @@ function buildDeck($suits, $cards) {
 // determine if a card is an ace
 // return true for ace, false for anything else
 function cardIsAce($card) {
-	if ($card == 'A') {
+	$face = substr($card, 0, 1);
+	if ($face == 'A') {
 		return true;
 	} else {
 		return false;
@@ -37,49 +38,57 @@ function cardIsAce($card) {
 // face cards are worth 10
 // numeric cards are worth their value
 function getCardValue($card) {
-	switch ($card) {
-		case '1':
-			$value = 1;
-			break;
+	$amount = substr($card, 0, 1);
+	switch ($amount) {
 		case '2':
 			$value = 2;
+			return $value;
 		case '3':
 			$value = 3;
+			return $value;
 		case '4':
 			$value = 4;
+			return $value;
 			break;
 		case '5':
 			$value = 5;
+			return $value;
 			break;
 		case '6':
 			$value = 6;
+			return $value;
 			break;
 		case '7':
 			$value = 7;
+			return $value;
 			break;
 		case '8':
 			$value = 8;
+			return $value;
 			break;
 		case '9':
 			$value = 9;
+			return $value;
 			break;
-		case '10':
+		case '1':
 			$value = 10;
+			return $value;
 			break;
 		case 'J':
 			$value = 10;
+			return $value;
 			break;
 		case 'Q':
 			$value = 10;
+			return $value;
 			break;
 		case 'K':
 			$value = 10;
+			return $value;
 			break;
 		case 'A':
 			$value = 11;
-			break;	
-		default:
-			# code...
+			return $value;
 			break;
 	}
 }
@@ -88,62 +97,75 @@ function getCardValue($card) {
 // don't forget to factor in aces
 // aces can be 1 or 11 (make them 1 if total value is over 21)
 function getHandTotal($hand) {
-  // todo
-}
+	$sum = 0;
+	foreach ($hand as $card) {
+		if (cardIsAce($card)) {
+			$ace = true;
+		}
+		$sum += getCardValue($card);
+	}
+	if ($ace == true && $sum > 21) {
+		$newSum = $sum - 10;
+		return $newSum;
+	} else {
+		return $sum;
+	}
+
+} print_r(getHandTotal(['A C', '5 D', 'A H']));
 
 // draw a card from the deck into a hand
 // pass by reference (both hand and deck passed in are modified)
 function drawCard(&$hand, &$deck) {
-  // todo
+	
 }
 
-// print out a hand of cards
-// name is the name of the player
-// hidden is to initially show only first card of hand (for dealer)
-// output should look like this:
-// Dealer: [4 C] [???] Total: ???
-// or:
-// Player: [J D] [2 D] Total: 12
-function echoHand($hand, $name, $hidden = false) {
-  // todo
-}
+// // print out a hand of cards
+// // name is the name of the player
+// // hidden is to initially show only first card of hand (for dealer)
+// // output should look like this:
+// // Dealer: [4 C] [???] Total: ???
+// // or:
+// // Player: [J D] [2 D] Total: 12
+// function echoHand($hand, $name, $hidden = false) {
+//   // todo
+// }
 
-// build the deck of cards
-$deck = buildDeck($suits, $cards);
+// // build the deck of cards
+// $deck = buildDeck($suits, $cards);
 
-// initialize a dealer and player hand
-$dealer = [];
-$player = [];
+// // initialize a dealer and player hand
+// $dealer = [];
+// $player = [];
 
-// dealer and player each draw two cards
-// todo
+// // dealer and player each draw two cards
+// // todo
 
-// echo the dealer hand, only showing the first card
-// todo
+// // echo the dealer hand, only showing the first card
+// // todo
 
-// echo the player hand
-// todo
+// // echo the player hand
+// // todo
 
-// allow player to "(H)it or (S)tay?" till they bust (exceed 21) or stay
-while (todo) {
-  // todo
-}
+// // allow player to "(H)it or (S)tay?" till they bust (exceed 21) or stay
+// while (todo) {
+//   // todo
+// }
 
-// show the dealer's hand (all cards)
-// todo
+// // show the dealer's hand (all cards)
+// // todo
 
-// todo (all comments below)
+// // todo (all comments below)
 
-// at this point, if the player has more than 21, tell them they busted
-// otherwise, if they have 21, tell them they won (regardless of dealer hand)
+// // at this point, if the player has more than 21, tell them they busted
+// // otherwise, if they have 21, tell them they won (regardless of dealer hand)
 
-// if neither of the above are true, then the dealer needs to draw more cards
-// dealer draws until their hand has a value of at least 17
-// show the dealer hand each time they draw a card
+// // if neither of the above are true, then the dealer needs to draw more cards
+// // dealer draws until their hand has a value of at least 17
+// // show the dealer hand each time they draw a card
 
-// finally, we can check and see who won
-// by this point, if dealer has busted, then player automatically wins
-// if player and dealer tie, it is a "push"
-// if dealer has more than player, dealer wins, otherwise, player wins
+// // finally, we can check and see who won
+// // by this point, if dealer has busted, then player automatically wins
+// // if player and dealer tie, it is a "push"
+// // if dealer has more than player, dealer wins, otherwise, player wins
 
 ?>
